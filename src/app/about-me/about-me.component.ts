786f0e30107aa5558d8671f92b4d849a27c29e2d
Rebecca Moura <rebeccamouradev@gmail.com>
2022-11-30 10:16:55 -0300

    about-me criado

--- src/app/about-me/about-me.component.ts
+++ src/app/about-me/about-me.component.ts
@@ -7,11 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutMeComponent implements OnInit {
 
-  bioText: string = 'Texto biografia blablabla';
+  allBioTexts: string[] = ['Seja bem-vindo ao meu mundo- ou um pequeno resumo dele. Estou dando meu melhor e colocando meu coração em cada aprendizado e projeto.', 'Seja bem-vindo ao meu mundo- ou um pequeno resumo dele. Minha história com a programação começou em um jogo e, desde então, sou apaixonada por desenvolvimento. Estudo todos os dias de maneira autodidata, sempre colocando meu coração em cada aprendizado e projeto.', 'Seja bem-vindo ao meu mundo- ou um pequeno resumo dele. Minha história com a programação começou em um jogo, que um amigo me disse que se semelhava bastante com lógica de programação. Eu, curiosa como sou, fui pesquisar sobre e, desde então, sou apaixonada por desenvolvimento. Dedico a maior parte dos meus dias para estudar, seja através de vídeoaulas ou lendo as documentações, sempre colocando meu coração em cada aprendizado e projeto. Mas minha trajetória não acaba aqui, ainda planejo me aprofundar mais e, futuramente, compartilhar meu conhecimento com o máximo de pessoas possível.'];
+  bioText: string = this.allBioTexts[0];
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  changeTextSize(event: any) {
+    this.bioText = this.allBioTexts[event.target.dataset.key]
   }
 
 }
