70c7de2f9ee9a1353a1c141fb8aa03a6ce1df717
Rebecca Moura <rebeccamouradev@gmail.com>
2022-12-07 11:15:04 -0300

    site finalizado

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -24,17 +24,40 @@ export class NavbarComponent implements OnInit {
   }
 
   changeMode(event: any) {
-    if (event.target.value === '1') {
-      document.body.classList.add('dark-theme')
-      return;
-    }
-
-    document.body.classList.remove('dark-theme')
+    event.target.value === '1' ? document.body.classList.add('dark-theme') : document.body.classList.remove('dark-theme')
   }
 
   menuToggle() {
     document.querySelector('.menu-toggle')?.classList.toggle('open')
     document.querySelector('.navbar-wrapper')?.classList.toggle('on')
     document.querySelector('.list-wrapper')?.classList.toggle('d-block')
+  }
+
+  scroll(event: any) {
+    if (event.target.classList?.contains('initial')) {
+      window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+      });
+
+      return;
+    }
+
+    if (event.target.classList?.contains('contact-us')) {
+      window.scrollTo({
+        top: 2087,
+        behavior: 'smooth'
+      });
+
+      return;
+    }
+
+    event.target.classList?.contains('skills') ? window.scrollTo({
+      top: 680,
+      behavior: 'smooth'
+    }) : window.scrollTo({
+      top: 1338,
+      behavior: 'smooth'
+    });
   }
 }
